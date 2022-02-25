782cb771e1aec1e1167db409f22f42e45948d2bb
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-02-25 07:43:11 +0000

    Add files via upload

--- modules/translate.js
+++ modules/translate.js
@@ -1,119 +1,45 @@
-const { MessageType } = require("@adiwajshing/baileys");
-const translate = require("@vitalets/google-translate-api");
-const inputSanitization = require("../sidekick/input-sanitization");
-const STRINGS = require("../lib/db");
-const format = require("python-format-js");
+const translate = require('translate-google-api')
+const defaultLang = 'en'
+const tld = 'cn'
 
-module.exports = {
-    name: "tr",
-    description: STRINGS.tr.DESCRIPTION,
-    extendedDescription: STRINGS.tr.EXTENDED_DESCRIPTION,
-    demo: {
-        isEnabled: true,
-        text: [
-            ".tr やめてください",
-            ".tr how are you | hindi",
-            ".tr how are you | hi",
-        ],
-    },
-    async handle(client, chat, BotsApp, args) {
-        const processing = await client.sendMessage(
-            BotsApp.chatId,
-            STRINGS.tr.PROCESSING,
-            MessageType.text
-        ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-        try {
-            var text = "";
-            var language = "";
-            if (args.length == 0) {
-                await client.sendMessage(
-                    BotsApp.chatId,
-                    STRINGS.tr.EXTENDED_DESCRIPTION,
-                    MessageType.text
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                return await client.deleteMessage(BotsApp.chatId, {
-                    id: processing.key.id,
-                    remoteJid: BotsApp.chatId,
-                    fromMe: true,
-                });
-            }
-            if (!BotsApp.isReply) {
-                try {
-                    var body = BotsApp.body.split("|");
-                    text = body[0].replace(
-                        BotsApp.body[0] + BotsApp.commandName + " ",
-                        ""
-                    );
-                    var i = 0;
-                    while (body[1].split(" ")[i] == "") {
-                        i++;
-                    }
-                    language = body[1].split(" ")[i];
-                } catch (err) {
-                    if (err instanceof TypeError) {
-                        text = BotsApp.body.replace(
-                            BotsApp.body[0] + BotsApp.commandName + " ",
-                            ""
-                        );
-                        language = "English";
-                    }
-                }
-            } else if (BotsApp.replyMessage) {
-                text = BotsApp.replyMessage;
-                language = args[0];
-            } else {
-                await client.sendMessage(
-                    BotsApp.chatId,
-                    STRINGS.tr.INVALID_REPLY,
-                    MessageType.text
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                return await client.deleteMessage(BotsApp.chatId, {
-                    id: processing.key.id,
-                    remoteJid: BotsApp.chatId,
-                    fromMe: true,
-                });
-            }
-            if (text.length > 4000) {
-                await client.sendMessage(
-                    BotsApp.chatId,
-                    STRINGS.tr.TOO_LONG.format(text.length),
-                    MessageType.text
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                return await client.deleteMessage(BotsApp.chatId, {
-                    id: processing.key.id,
-                    remoteJid: BotsApp.chatId,
-                    fromMe: true,
-                });
-            }
-            await translate(text, {
-                to: language,
-            })
-                .then((res) => {
-                    client.sendMessage(
-                        BotsApp.chatId,
-                        STRINGS.tr.SUCCESS.format(
-                            res.from.language.iso,
-                            language,
-                            res.text
-                        ),
-                        MessageType.text
-                    );
-                })
-                .catch((err) => {
-                    inputSanitization.handleError(
-                        err,
-                        client,
-                        BotsApp,
-                        STRINGS.tr.LANGUAGE_NOT_SUPPORTED
-                    );
-                });
-            return await client.deleteMessage(BotsApp.chatId, {
-                id: processing.key.id,
-                remoteJid: BotsApp.chatId,
-                fromMe: true,
-            });
-        } catch (err) {
-            inputSanitization.handleError(err, client, BotsApp);
-        }
-    },
-};
+let handler = async (m, { args, usedPrefix, command }) => {
+    let err = `
+Example:
+${usedPrefix + command} <lang> [text]
+${usedPrefix + command} id your messages
+
+List of supported languages: https://cloud.google.com/translate/docs/languages
+`.trim()
+
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+    let result
+    try {
+        result = await translate(`${text}`, {
+            tld,
+            to: lang,
+        })
+    } catch (e) {
+        result = await translate(`${text}`, {
+            tld,
+            to: defaultLang,
+        })
+        throw err
+    } finally {
+        m.reply(result[0])
+    }
+
+}
+handler.help = ['translate'].map(v => v + ' <lang> <text>')
+handler.tags = ['tools']
+handler.command = /^(tr(anslate)?)$/i
+handler.limit = false
+handler.fail = null
+handler.exp = 0
+module.exports = handler
--- modules/tts.js
+++ modules/tts.js
@@ -1,44 +1,43 @@
-const { MessageType, Mimetype } = require("@adiwajshing/baileys")
-const format = require('python-format-js');
-const googleTTS = require('google-tts-api');
-const STRINGS = require("../lib/db.js");
+let gtts = require('node-gtts')
+let fs = require('fs')
+let path = require('path')
+let { spawn } = require('child_process')
 
-module.exports = {
-    name: "tts",
-    description: STRINGS.tts.DESCRIPTION,
-    extendedDescription: STRINGS.tts.EXTENDED_DESCRIPTION,
-    demo: {isEnabled: true, text: ['.tts やめてください', '.tts やめてください | ja']},
-    async handle(client, chat, BotsApp, args) {
-        const proccessing = await client.sendMessage(BotsApp.chatId, STRINGS.tts.PROCESSING, MessageType.text);
-        let text = '';
-        let langCode = "en";
-        for (var i = 0; i < args.length; i++) {
-            if (args[i] == '|') {
-                langCode = args[i + 1];
-                break;
-            }
-            text += args[i] + " ";
-        }
-        if(text === ""){
-            await client.sendMessage(BotsApp.chatId, STRINGS.tts.NO_INPUT, MessageType.text);
-            return await client.deleteMessage(BotsApp.chatId, { id: proccessing.key.id, remoteJid: BotsApp.chatId, fromMe: true });
-        }
-        if(text.length > 200){
-            await client.sendMessage(BotsApp.chatId, STRINGS.tts.TOO_LONG.format(text.length), MessageType.text);
-        }else{
-            try {
-                const url = googleTTS.getAudioUrl(text, {
-                    lang: langCode,
-                    slow: false,
-                    host: 'https://translate.google.com',
-                });
-                // console.log(url);
-                await client.sendMessage(BotsApp.chatId, { url: url }, MessageType.audio, { mimetype: Mimetype.mp4Audio });
-            }
-            catch (err) {
-                // console.log(err);
-            }
-        }
-        return await client.deleteMessage (BotsApp.chatId, {id: proccessing.key.id, remoteJid: BotsApp.chatId, fromMe: true});
-    }
+const defaultLang = 'hi'
+let handler = async (m, { conn, args }) => {
+
+  let lang = args[0]
+  let text = args.slice(1).join(' ')
+  if ((args[0] || '').length !== 2) {
+    lang = defaultLang
+    text = args.join(' ')
+  }
+  if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+  let res
+  try { res = await tts(text, lang) }
+  catch (e) {
+    m.reply(e + '')
+    res = await tts(text)
+  } finally {
+    conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+  }
+}
+handler.help = ['tts <lang> <text>']
+handler.tags = ['tools']
+handler.command = /^g?tts$/i
+module.exports = handler
+
+function tts(text, lang = 'id') {
+  console.log(lang, text)
+  return new Promise((resolve, reject) => {
+    try {
+      let tts = gtts(lang)
+      let filePath = path.join(__dirname, '../tmp', (1 * new Date) + '.wav')
+      tts.save(filePath, text, () => {
+        resolve(fs.readFileSync(filePath))
+        fs.unlinkSync(filePath)
+      })
+    } catch (e) { reject(e) }
+  })
 }
