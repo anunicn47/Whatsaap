a247a3e3c40e6d0438b47f1e7af37e712c0a8a8e
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-02-25 07:39:33 +0000

    Add files via upload

--- modules/sticker.js
+++ modules/sticker.js
@@ -1,150 +1,40 @@
-const { MessageType, MimetypeMap } = require("@adiwajshing/baileys");
-const ffmpeg = require("fluent-ffmpeg");
-const fs = require("fs");
-const inputSanitization = require("../sidekick/input-sanitization");
-const { JSDOM } = require("jsdom");
-const { window } = new JSDOM();
+const { MessageType } = require('@adiwajshing/baileys')
+const { sticker } = require('../lib/sticker')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw `reply to image with caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) return m.reply('Maximum 10 seconds!')
+      let img = await q.download()
+      if (!img) throw `reply video/gif with caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (/webp/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw `reply sticker with caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('Inavlid URL!')
+    }
+  } finally {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+    else throw 'Conversion failed'
+  }
+}
+handler.help = ['sticker (caption|reply media)', 'sticker <url>', 'stickergif (caption|reply media)', 'stickergif <url>']
+handler.tags = ['sticker']
+handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
-const Strings = require("../lib/db");
-const STICKER = Strings.sticker;
+module.exports = handler
 
-module.exports = {
-    name: "sticker",
-    description: STICKER.DESCRIPTION,
-    extendedDescription: STICKER.EXTENDED_DESCRIPTION,
-    demo: { isEnabled: false },
-    async handle(client, chat, BotsApp, args) {
-        // Task starts here
-        var startTime = window.performance.now();
-        try {
-            // Function to convert media to sticker
-            const convertToSticker = async (imageId, replyChat) => {
-                var downloading = await client.sendMessage(
-                    BotsApp.chatId,
-                    STICKER.DOWNLOADING,
-                    MessageType.text
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-
-                const fileName = "./tmp/convert_to_sticker-" + imageId;
-                const filePath = await client.downloadAndSaveMediaMessage(
-                    replyChat,
-                    fileName
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                const stickerPath = "./tmp/st-" + imageId + ".webp";
-                if (BotsApp.type === "image" || BotsApp.isReplyImage) {
-                    ffmpeg(filePath)
-                        .outputOptions(["-y", "-vcodec libwebp"])
-                        .videoFilters(
-                            "scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1"
-                        )
-                        .save(stickerPath)
-                        .on("end", async () => {
-                            await client.sendMessage(
-                                BotsApp.chatId,
-                                fs.readFileSync(stickerPath),
-                                MessageType.sticker
-                            ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                            inputSanitization.deleteFiles(
-                                filePath,
-                                stickerPath
-                            );
-                            inputSanitization.performanceTime(startTime);
-                            await client.deleteMessage(BotsApp.chatId, {
-                                id: downloading.key.id,
-                                remoteJid: BotsApp.chatId,
-                                fromMe: true,
-                            }).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                        })
-                        .on('error', async() => {
-                            inputSanitization.handleError(err, client, BotsApp)
-                            await client.deleteMessage(BotsApp.chatId, {
-                                id: downloading.key.id,
-                                remoteJid: BotsApp.chatId,
-                                fromMe: true,
-                            }).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                        });
-                    return;
-                }
-                ffmpeg(filePath)
-                    .duration(8)
-                    .outputOptions([
-                        "-y",
-                        "-vcodec libwebp",
-                        "-lossless 1",
-                        "-qscale 1",
-                        "-preset default",
-                        "-loop 0",
-                        "-an",
-                        "-vsync 0",
-                        "-s 600x600",
-                    ])
-                    .videoFilters(
-                        "scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1"
-                    )
-                    .save(stickerPath)
-                    .on("end", async (err) => {
-                        await client.sendMessage(
-                            BotsApp.chatId,
-                            fs.readFileSync(stickerPath),
-                            MessageType.sticker
-                        ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                        inputSanitization.deleteFiles(filePath, stickerPath);
-                        inputSanitization.performanceTime(startTime);
-                        await client.deleteMessage(BotsApp.chatId, {
-                            id: downloading.key.id,
-                            remoteJid: BotsApp.chatId,
-                            fromMe: true,
-                        }).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                    })
-                    .on('error', async(err) => {
-                        inputSanitization.handleError(err, client, BotsApp)
-                        await client.deleteMessage(BotsApp.chatId, {
-                            id: downloading.key.id,
-                            remoteJid: BotsApp.chatId,
-                            fromMe: true,
-                        }).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                    });
-                return;
-            };
-
-            // User sends media message along with command in caption
-            if (BotsApp.isImage || BotsApp.isGIF || BotsApp.isVideo) {
-                var replyChatObject = {
-                    message: chat.message,
-                };
-                var imageId = chat.key.id;
-                convertToSticker(imageId, replyChatObject);
-            }
-            // Replied to an image , gif or video
-            else if (
-                BotsApp.isReplyImage ||
-                BotsApp.isReplyGIF ||
-                BotsApp.isReplyVideo
-            ) {
-                var replyChatObject = {
-                    message:
-                        chat.message.extendedTextMessage.contextInfo
-                            .quotedMessage,
-                };
-                var imageId =
-                    chat.message.extendedTextMessage.contextInfo.stanzaId;
-                convertToSticker(imageId, replyChatObject);
-            } else {
-                client.sendMessage(
-                    BotsApp.chatId,
-                    STICKER.TAG_A_VALID_MEDIA_MESSAGE,
-                    MessageType.text
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                inputSanitization.performanceTime(startTime);
-            }
-            return;
-        } catch (err) {
-            await inputSanitization.handleError(
-                err,
-                client,
-                BotsApp,
-                STICKER.TAG_A_VALID_MEDIA_MESSAGE
-            );
-        }
-    },
-};
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
